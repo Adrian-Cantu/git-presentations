1723b566166c979220815bc9ace48535623fd987
Ubuntu <ubuntu@ip-172-31-6-37.us-west-2.compute.internal>
2018-11-30 08:07:17 +0000

    was ablre to reproduce victors results. adde a parser for fasta

--- keras/phage_protein/shuff.sh
+++ keras/phage_protein/shuff.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-cat pi_dipep | shuf > pi_dipep_shuf
+gunzip -c pi_dipep.gz | shuf > pi_dipep_shuf
 head -n 24000 pi_dipep_shuf > pi_dipep_head
 head -n 10 pi_dipep_shuf > pi_dipep_mini_head
 tail -n 1675 pi_dipep_shuf > pi_dipep_tail
 
-cat tripep_data | shuf > tripep_shuf
+gunzip -c tripep_data.gz | shuf > tripep_shuf
 head -n 24000 tripep_shuf > tripep_head
 tail -n 1675 tripep_shuf > tripep_tail
 
