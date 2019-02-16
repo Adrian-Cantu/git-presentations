ed1995a1cd9cd50b8694a4f19461279f1baea4a1
Adrian-Cantu <garbanyo@gmail.com>
2019-02-16 04:24:17 +0000

    added initialization script

---
+++ keras/phage_protein/phage_init.py
@@ -0,0 +1,6 @@
+import os
+#cwd = os.getcwd()
+path = os.path.abspath(__file__)
+root_dir = os.path.dirname(path)
+fasta_dir = os.path.join(root_dir, 'fasta')
+model_dir = os.path.join(root_dir, 'model')
