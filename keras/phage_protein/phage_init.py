94390c246ec3f33c7a10465834e27503642e432c
Adrian Cantu <garbanyo@gmail.com>
2019-03-12 22:02:09 +0000

    added code to train using all features

--- keras/phage_protein/phage_init.py
+++ keras/phage_protein/phage_init.py
@@ -4,3 +4,5 @@ path = os.path.abspath(__file__)
 root_dir = os.path.dirname(path)
 fasta_dir = os.path.join(root_dir, 'fasta')
 model_dir = os.path.join(root_dir, 'model')
+data_dir =  os.path.join(root_dir, 'data')
+
