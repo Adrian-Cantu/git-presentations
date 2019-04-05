1bdc7e23ea9d8045c3b65aaab6c7ce396d3dd16f
Adrian Cantu <garbanyo@gmail.com>
2019-04-05 12:35:35 -0700

    add list files

--- keras/phage_protein/phage_init.py
+++ keras/phage_protein/phage_init.py
@@ -5,4 +5,4 @@ root_dir = os.path.dirname(path)
 fasta_dir = os.path.join(root_dir, 'fasta')
 model_dir = os.path.join(root_dir, 'model')
 data_dir =  os.path.join(root_dir, 'data')
-
+fasta_dir_2 = os.path.join(root_dir, 'download_seq/all/clustered/')
